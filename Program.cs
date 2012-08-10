9caca53216b09406fe93b6914a4e651271cc8ce0
Md Latifur Rahman Maruf <marufbd@gmail.com>
2012-08-10 17:34:08 +1000

    vs 2010 console skeleton

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KnightsTravails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Console program for knights travails problem");
+        }
+    }
+}
