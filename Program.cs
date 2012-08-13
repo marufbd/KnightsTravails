4e9f64e9f8f13abac29b34943f447c48e2dc40e6
Md. Latifur Rahman Maruf <marufbd@gmail.com>
2012-08-13 21:20:17 +1000

    finalising console input output

--- Program.cs
+++ Program.cs
@@ -10,11 +10,11 @@ namespace KnightsTravails
         static void Main(string[] args)
         {
             Console.WriteLine("==== Console program for knights travails problem =====\n\n");
-
-            Console.WriteLine("Please enter source and destination(algenraic notation) \nseparated by space(i.e a8 b7):\n");
+            
+            Console.WriteLine("Please enter source and destination(algebraic notation) \nseparated by space(i.e a8 b7):\n");
             string inp = Console.ReadLine();
 
-            if (!String.IsNullOrEmpty(inp))
+            while (!String.IsNullOrEmpty(inp) && !inp.ToLowerInvariant().Equals("q") )
             {                
                 string[] inpSquares = inp.Split(' ');
                 string src = inpSquares[0].Trim().ToLowerInvariant();
@@ -26,14 +26,23 @@ namespace KnightsTravails
                 var shortestPathStrategy = new DijkstraStrategy(graph: knightGraph, source: board.Squares[src]);
                 var shortestPath = shortestPathStrategy.GetShortestPathTo(board.Squares[dest]);
 
-                Console.WriteLine(String.Format("Shortest Path from {0} to {1}", src, dest));
+                Console.WriteLine(String.Format("Shortest Path from {0} to {1} : ", src, dest));
                 foreach (var square in shortestPath)
                 {
-                    Console.Write(square+" ");
+                    Console.Write(square + " ");
                 }
+                
+                Console.Write("\nWant to continue?(y/n)");
+                string cont = Console.ReadLine();
+                if(!cont.ToLowerInvariant().Equals("y"))
+                    break;
 
-                Console.WriteLine();
-            }            
+                Console.WriteLine("\nEnter source and destination separated by space");
+                inp = Console.ReadLine();
+            }
+
+            Console.WriteLine();
         }
+
     }
 }
